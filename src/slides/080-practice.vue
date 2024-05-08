8ba387d9b5ddf828a860c508a28783dea8fde032
Juan Carlos Corona Romero <juan.carloscoronaromero@vidyard.com>
2024-05-08 12:52:05 -0400

    Add the practice slides again

--- src/slides/080-practice.vue
+++ src/slides/080-practice.vue
@@ -3,6 +3,13 @@ import tailwindLogo from '../assets/tailwind.svg'
 </script>
 
 <template>
-  <img :src="tailwindLogo" />
-  <h1>Tailwind CSS</h1>
+  <h1>Tailwind CSS!</h1>
+  <!-- <h1 class="flex flex-col sm:flex-row gap-4">
+    <img class="not-prose h-16 w-min" :src="tailwindLogo" />
+    <span>Tailwind CSS!</span>
+  </h1> -->
+
+  <!-- <img class="not-prose h-16 sm:float-left mb-4 sm:mr-4" :src="tailwindLogo" /> -->
+
+  <!-- <p class="lead clear-both">Making UIs all day long</p> -->
 </template>
